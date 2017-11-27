fed740e38d861054f554c590ae36311ad499e082
Chris McCullough <mcculloughjchris@gmail.com>
2017-11-27 08:18:43 -0500

    separated react from the script, can now make pure rails apps or choose react and bootstrap

--- files/application.js
+++ files/application.js
@@ -10,7 +10,4 @@
 import '../scss/main.scss'
 
 console.log('Hello World from Webpacker')
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
+
